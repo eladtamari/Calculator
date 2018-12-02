58eb7fef4a0694f1d93f7b19d9749799622e7543
Elad <tamari.elad@infinityar.com>
2018-12-02 12:25:45 +0200

    initialize proj in git

---
+++ Calculator/Calculator.Test.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        [TestFixture]
+
+        class MyTestCase
+        {
+            [TestCase]
+            public void Add()
+            {
+                myMath math = new myMath();
+                Assert.AreEqual(31, math.Add(11, 20));
+            }
+
+            [TestCase]
+            public void Add_()
+            {
+                myMath math = new myMath();
+                Assert.AreEqual(31, math.Add(111, 20));
+            }
+        }
+    }
+}
---
+++ Calculator/myMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class myMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+
+        }
+        }
+}
